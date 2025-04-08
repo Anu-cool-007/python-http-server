ac3fe95d4e56f127a52541ded0a9b7aa619cf7e8
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2025-04-08 23:52:01 +0530

    handle base route with 200 response

--- app/main.py
+++ app/main.py
@@ -7,8 +7,11 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
+        connection, address = server_socket.accept()
+        print(address)
+
+        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
