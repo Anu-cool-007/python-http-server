3621ee5ad2604f93c132a67ed21b854debe42a1b
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2025-04-09 00:58:28 +0530

    Added support for concurrent requests

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 import socket
+import threading
 
 from app.router import handle_request
 from app.utils import decode_request, encode_response
 
+
+def handler(client_socket):
+    with client_socket:
+        data = client_socket.recv(1024)
+        request = decode_request(data)
+
+        response = handle_request(request)
+
+        client_socket.sendall(encode_response(response))
+        print(f"Sent response to {client_socket.getpeername()}")
+        print(f"Response: {response}")
+        print(f"Request: {request}")
+        print(f"Headers: {request.headers}")
+        print(f"Body: {request.body}")
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,15 +26,11 @@ def main():
     # Uncomment this to pass the first stage
     #
     with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
-        connection, address = server_socket.accept()
-        print(address)
-
-        data = connection.recv(1024)
-        request = decode_request(data)
-
-        response = handle_request(request)
-
-        connection.sendall(encode_response(response))
+        while True:
+            client_soc, client_address = server_socket.accept()
+            # Send each "client_soc" connection as a parameter to a thread.
+            threading.Thread(target=handler,args=(client_soc,), daemon=True).start()
+            print(f"Accepted connection from {client_address}")
 
 
 if __name__ == "__main__":
