f03bcc7efb761693846e07abdaeb6b4079cdd1f2
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2025-04-09 09:52:05 +0530

    added file handler route

--- app/main.py
+++ app/main.py
@@ -1,27 +1,36 @@
+import argparse
 import socket
 import threading
+
+from argparse import Namespace
+from socket import socket as Socket
 
 from app.router import handle_request
 from app.utils import decode_request, encode_response
 
 
-def handler(client_socket):
+
+def handler(client_socket: Socket, args: Namespace):
     with client_socket:
         data = client_socket.recv(1024)
         request = decode_request(data)
 
-        response = handle_request(request)
+        response = handle_request(request, args)
 
-        client_socket.sendall(encode_response(response))
-        print(f"Sent response to {client_socket.getpeername()}")
-        print(f"Response: {response}")
         print(f"Request: {request}")
         print(f"Headers: {request.headers}")
         print(f"Body: {request.body}")
+        print(f"Response: {response}")
+        client_socket.sendall(encode_response(response))
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
+
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--directory", help="Specify the file directory")
+
+    args = parser.parse_args()
 
     # Uncomment this to pass the first stage
     #
@@ -29,8 +38,8 @@ def main():
         while True:
             client_soc, client_address = server_socket.accept()
             # Send each "client_soc" connection as a parameter to a thread.
-            threading.Thread(target=handler,args=(client_soc,), daemon=True).start()
             print(f"Accepted connection from {client_address}")
+            threading.Thread(target=handler, args=(client_soc, args), daemon=True).start()
 
 
 if __name__ == "__main__":
--- app/router.py
+++ app/router.py
@@ -1,8 +1,11 @@
+from argparse import Namespace
+import os
+from pathlib import Path
 
 from app.models import HttpRequest, HttpResponse
 
 
-def handle_request(request: HttpRequest) -> HttpResponse:
+def handle_request(request: HttpRequest, args: Namespace) -> HttpResponse:
 
     match request.target.split("/")[1:]:
         case [""]:
@@ -10,7 +13,7 @@ def handle_request(request: HttpRequest) -> HttpResponse:
                 status_code=200,
                 status_text="OK",
                 headers={"Content-Type": "text/plain"},
-                body="<h1>Hello, World!</h1>",
+                body="Hello, World!",
             )
         case ["echo", name]:
             return HttpResponse(
@@ -27,10 +30,40 @@ def handle_request(request: HttpRequest) -> HttpResponse:
                 headers={"Content-Type": "text/plain", "Content-Length": str(len(user_agent))},
                 body=f"{user_agent}",
             )
+        case ["files", filename]:
+            base_path = vars(args).get("directory")
+            if not base_path:
+                return HttpResponse(
+                    status_code=500,
+                    status_text="Internal Server Error",
+                    headers={},
+                    body="No directory specified",
+                )
+            path = Path(os.path.join(base_path, filename))
+
+            if path.is_file():
+                with open(path, "rb") as file:
+                    content = file.read()
+                return HttpResponse(
+                    status_code=200,
+                    status_text="OK",
+                    headers={
+                        "Content-Type": "application/octet-stream",
+                        "Content-Length": str(len(content)),
+                    },
+                    body=content.decode(),
+                )
+            else:
+                return HttpResponse(
+                    status_code=404,
+                    status_text="Not Found",
+                    headers={},
+                    body="404 Not Found",
+                )
         case _:
             return HttpResponse(
                 status_code=404,
                 status_text="Not Found",
                 headers={},
-                body="<h1>404 Not Found</h1>",
+                body="404 Not Found",
             )
