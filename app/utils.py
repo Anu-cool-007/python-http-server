0251f8ea91a0d9df55aa8c363381ad3ad04643ce
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2025-04-09 10:06:36 +0530

    fixed response type bug

--- app/utils.py
+++ app/utils.py
@@ -32,13 +32,16 @@ def encode_response(response: HttpResponse, encoding: str) -> bytes:
     """
 
     if encoding == "gzip":
-        compressed_response = gzip.compress(response.body.encode())
-        response.body = compressed_response.decode()
+        response.body = gzip.compress(response.body.encode())
         response.headers["Content-Encoding"] = "gzip"
-        response.headers["Content-Length"] = str(len(compressed_response))
+        response.headers["Content-Length"] = str(len(response.body))
 
     status_line = f"HTTP/1.1 {response.status_code} {response.status_text}\r\n"
     headers = "".join([f"{key}: {value}\r\n" for key, value in response.headers.items()])
     body = response.body
-    return (status_line + headers + "\r\n" + body).encode()
+
+    if isinstance(body, str):
+        body = body.encode()
+
+    return (status_line + headers + "\r\n").encode() + body
 
